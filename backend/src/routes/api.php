c940c5332352013058359e73cf29fb7cf3806903
Uday Chauhan <152022761+UdayChauhan8@users.noreply.github.com>
2025-12-12 11:35:22 +0530

    Delete backend directory

--- backend/src/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\JoinController;
-
-Route::prefix('api/v1')->group(function () {
-    Route::post('/clinics/{clinic_slug}/join', [JoinController::class, 'join']);
-    Route::get('/clinics/{clinic_slug}/status/{token_id}', [JoinController::class, 'status']);
-});
